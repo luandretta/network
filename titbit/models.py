752bc2e42eb63e3c27c6162c0939d00b1e7c03d5
Lucimeri Andretta <luandretta@hotmail.com>
2023-04-28 09:58:18 +0000

    Fix: background in profile model

--- titbit/models.py
+++ titbit/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from cloudinary.models import CloudinaryField
-from django.db.models.signal import post_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
@@ -40,7 +40,25 @@ class UserProfile(models.Model):
                                   default='user.png',
                                   blank=True)
     bg_pic = CloudinaryField('Background Picture',
-                              default='bg.png',
-                              blank=True)
+                             default='bg.png',
+                             blank=True)
     birth_date = models.DateField(null=True, blank=True)
     location = models.CharField(max_length=100, blank=True, null=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Once the User is saved in the database, the profile object will be created
+    Create the instance
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """
+    Save the instance object (profile)
+    """
+    instance.profile.save()
