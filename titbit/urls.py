2de597035e162db3be6cc6e9063da04d568a588b
Lucimeri Andretta <luandretta@hotmail.com>
2023-04-29 15:24:31 +0000

    Feat: create follow/unfollow view
    Edit profile templates to display follow/unfollow button

--- titbit/urls.py
+++ titbit/urls.py
@@ -1,18 +1,23 @@
 from django.urls import path
 from .views import (
     AllPostsListView, PostDetailView, PostEditView, PostDeleteView,
-    CommentDeleteView, ProfileView, ProfileEditView)
+    CommentDeleteView, ProfileView, ProfileEditView, AddFollower,
+    RemoveFollower)
 
 
 urlpatterns = [
-    path('', AllPostsListView.as_view(), name='post-list'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
-    path('post/delete/<int:pk>/', PostDeleteView.as_view(),
-         name='post-delete'),
-    path('post/<int:post_pk>/comment/delete<int:pk>/',
-         CommentDeleteView.as_view(), name='comment-delete'),
-    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
-    path('profile/edit/<int:pk>/', ProfileEditView.as_view(),
-         name='profile-edit'),
+     path('', AllPostsListView.as_view(), name='post-list'),
+     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+     path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
+     path('post/delete/<int:pk>/', PostDeleteView.as_view(),
+          name='post-delete'),
+     path('post/<int:post_pk>/comment/delete<int:pk>/',
+          CommentDeleteView.as_view(), name='comment-delete'),
+     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+     path('profile/edit/<int:pk>/', ProfileEditView.as_view(),
+          name='profile-edit'),
+     path('profile/<int:pk>/followers/add', AddFollower.as_view(),
+          name='add-follower'),
+     path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(),
+          name='remove-follower'),
 ]
--- titbit/views.py
+++ titbit/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.views import View
@@ -134,18 +134,35 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 class ProfileView(LoginRequiredMixin, View):
     """
-    View from Profile
+    View from User's Profile
     Each Profile has a unique url through pk
+    Displays posts and followers from User
     """
     def get(self, request, pk, *args, **kwargs):
         profile = UserProfile.objects.get(pk=pk)
         user = profile.user
         posts = Post.objects.filter(author=user).order_by('-posted_on')
 
+        followers = profile.followers.all()
+        
+        if len(followers) == 0:
+            is_following = False
+
+        for follower in followers:
+            if follower == request.user:
+                is_following = True
+                break
+            else:
+                is_following = False
+
+        number_of_followers = len(followers)
+
         context = {
             'user': user,
             'profile': profile,
-            'posts': posts
+            'posts': posts,
+            'number_of_followers': number_of_followers,
+            'is_following': is_following,
         }
 
         return render(request, 'titbit/profile.html', context)
@@ -166,3 +183,25 @@ class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         profile = self.get_object()
         return self.request.user == profile.user
+
+
+class AddFollower(LoginRequiredMixin, View):
+    """
+    Follow another User
+    """
+    def post(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        profile.followers.add(request.user)
+
+        return redirect('profile', pk=profile.pk)
+
+
+class RemoveFollower(LoginRequiredMixin, View):
+    """
+    Unfollow an User
+    """
+    def post(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        profile.followers.remove(request.user)
+
+        return redirect('profile', pk=profile.pk)
