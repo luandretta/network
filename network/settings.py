2e169d9216b755b6c2051bc0bd7caa0b5e67b885
Lucimeri Andretta <luandretta@hotmail.com>
2023-05-11 19:36:39 +0000

    Fix: collestatic due heroku error

--- network/settings.py
+++ network/settings.py
@@ -175,8 +175,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'titbit/static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+print(STATICFILES_DIRS)
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
