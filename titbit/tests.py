295f09cc27a08a0a1d8027249c9dce817110bf0c
Lucimeri Andretta <luandretta@hotmail.com>
2023-05-21 20:15:43 +0000

    Test: create test_delete_post
    Set the database to elephant at settings back

--- titbit/tests.py
+++ titbit/tests.py
@@ -1,25 +1,38 @@
 from django.test import TestCase
 from django.test import Client
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.shortcuts import render
 from django.contrib.auth.models import User
-import json
+from http import HTTPStatus
+from titbit.models import Post
 
 
-from titbit.models import Comment, UserProfile, Post
-
-
-class TestAllPosts(TestCase):
+class TestPosts(TestCase):
     def setUp(self) -> None:
         return super().setUp()
         self.client = Client()
 
-    def test_all_posts_list_view(self):
+    def test_all_posts_list_view_post(self):
         message = "TESTE"
-        marlos = User(username="marlos")
-        marlos.save()
-        post = Post(author=marlos, content=message)
+        user_test = User(username="user_test")
+        user_test.save()
+        post = Post(author=user_test, content=message)
         post.save()
+        self.client.force_login(user_test)
+        response = self.client.post(reverse('post-list'),
+                                    {"content": message, "author": user_test})
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTrue(post.content.__contains__("TESTE"))
-        response = self.client.get(reverse('post-list'))
-        self.assertEquals(response.status_code, 302)
+
+    def test_delete_post(self):
+        message = "TESTE"
+        user_test = User(username="user_test")
+        user_test.save()
+        post = Post(author=user_test, content=message)
+        post.save()
+        self.client.force_login(user_test)
+        response = self.client.delete(reverse("post-delete",
+                                      kwargs={"pk": post.id}))
+        self.assertEqual(response.status_code, 302)
+        # redirect to "home"
+        self.assertRedirects(response, "/titbit/", status_code=302)
