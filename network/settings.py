7da1494589f7397f1551077830c38e294a7d869d
Lucimeri Andretta <luandretta@hotmail.com>
2023-05-21 16:44:35 +0000

    Test: all posts list

--- network/settings.py
+++ network/settings.py
@@ -115,15 +115,16 @@ WSGI_APPLICATION = 'network.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- titbit/tests.py
+++ titbit/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from django.shortcuts import render
+from django.contrib.auth.models import User
+import json
 
-# Create your tests here.
+
+from titbit.models import Comment, UserProfile, Post
+
+
+class TestAllPosts(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+        self.client = Client()
+
+    def test_all_posts_list_view(self):
+        message = "TESTE"
+        marlos = User(username="marlos")
+        marlos.save()
+        post = Post(author=marlos, content=message)
+        post.save()
+        self.assertTrue(post.content.__contains__("TESTE"))
+        response = self.client.get(reverse('post-list'))
+        self.assertEquals(response.status_code, 302)
